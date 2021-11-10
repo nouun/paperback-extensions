e04eb31ac49f6ddf09be186f8cb6fe29d8343eec
nouun <me@nouun.dev>
2021-11-10 08:28:31 +0000

    feat(ComicK): add new source

---
+++ src/ComicK/ComicK.spec.ts
@@ -0,0 +1,103 @@
+import {
+  APIWrapper,
+  Manga,
+  MangaTile,
+  SearchRequest,
+  Source,
+} from "paperback-extensions-common";
+
+import { ComicK } from "./ComicK";
+import cheerio from "cheerio";
+
+import chai = require("chai");
+import chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+
+describe("ComicK.Fun Tests", function() {
+  this.timeout(15000);
+  const wrapper: APIWrapper = new APIWrapper();
+  const source: Source = new ComicK(cheerio);
+  const mangaId = "komi-san-wa-komyushou-desu";
+
+  it("Search Manga Title", async () => {
+    const testSearch: SearchRequest = { title: "solo" };
+
+    const search = await wrapper.searchRequest(source, testSearch);
+
+    for (const result of search.results) {
+      chai.expect(result, "No response from server").to.exist;
+
+      const res = <MangaTile>result;
+      chai.expect(res.id, "No ID found for search query").to.be.not.empty;
+      chai.expect(res.image, "No image found for search").to.be.not.empty;
+      chai.expect(res.title, "No title").to.be.not.null;
+      chai.expect(res.subtitleText, "No subtitle text").to.be.not.null;
+      chai.expect(res.primaryText, "No primary text").to.be.not.null;
+      chai.expect(res.secondaryText, "No secondary text").to.be.not.null;
+    }
+  });
+
+  //   it("Search Manga Genre", async () => {
+  //     const testSearch: SearchRequest = {
+  //       includedTags: [{
+  //         id: "19",
+  //         label: "Romance"
+  //       }]
+  //     };
+
+  //     const search = await wrapper.searchRequest(source, testSearch);
+
+  //     for (const result of search.results) {
+  //       chai.expect(result, "No response from server").to.exist;
+
+  //       const res = <MangaTile> result;
+  //       chai.expect(res.id, "No ID found for search query").to.be.not.empty;
+  //       chai.expect(res.image, "No image found for search").to.be.not.empty;
+  //       chai.expect(res.title, "No title").to.be.not.null;
+  //       chai.expect(res.subtitleText, "No subtitle text").to.be.not.null;
+  //       chai.expect(res.primaryText, "No primary text").to.be.not.null;
+  //       chai.expect(res.secondaryText, "No secondary text").to.be.not.null;
+  //     }
+  //   });
+
+  it("Retrieve Manga Details", async () => {
+    const data = <Manga & { id: number }>await wrapper.getMangaDetails(source, mangaId);
+
+    chai.expect(data.id, "Missing ID").to.be.not.empty;
+    chai.expect(data.image, "Missing Image").to.be.not.empty;
+    chai.expect(data.artist, "Missing Artist").to.be.not.empty;
+  });
+
+  it("Get Chapters", async () => {
+    const data = await wrapper.getChapters(source, mangaId);
+
+    chai.expect(data, "No chapters present for: [" + mangaId + "]").to.not.be.empty;
+  });
+
+  it("Get Chapter Details", async () => {
+    const chapters = await wrapper.getChapters(source, mangaId);
+
+    const chapter = chapters[0];
+    chai.expect(chapter, "No server response").to.exist;
+    if (!chapter) return;
+
+    const data = await wrapper.getChapterDetails(source, mangaId, chapter.id);
+
+    chai.expect(data, "No server response").to.exist;
+    chai.expect(data, "Empty server response").to.not.be.empty;
+
+    chai.expect(data.id, "Missing ID").to.be.not.empty;
+    chai.expect(data.mangaId, "Missing hentaiId").to.be.not.empty;
+    chai.expect(data.pages, "No pages present").to.be.not.empty;
+  });
+
+  //   it("Get Tags", async () => {
+  //     const data = await wrapper.getTags(source);
+
+  //     const cats = <TagSection> (<TagSection[]> data)[0];
+  //     chai.expect(cats, "No server response").to.exist;
+  //     chai.expect(cats.tags, "Empty server repsones").to.not.be.empty;
+
+  //   });
+});
---
+++ src/ComicK/ComicKParser.ts
@@ -0,0 +1,114 @@
+import {
+  CKChapterPreview,
+  CKGenre,
+  CKPerson,
+  CKRelated,
+  CKSearchResult,
+} from "./ComicKTypes";
+import {
+  Chapter,
+  LanguageCode,
+  Manga,
+  MangaTile,
+  TagSection,
+} from "paperback-extensions-common";
+import { CKLanguages } from "./ComicKHelper";
+import { StateData } from "./ComicKSettings";
+
+export const parseSearchTags = (genre: string): TagSection[] => {
+  const genreTags = JSON.parse(genre)
+    .filter((item: CKGenre) => item.slug && item.name)
+    .map((item: CKGenre) => createTag({
+      id: item.slug,
+      label: item.name,
+    }));
+
+  return [
+    createTagSection({
+      id: "genre",
+      label: "Genre",
+      tags: genreTags,
+    }),
+  ];
+};
+
+export const parseMangaDetails = (mangaId: string, data: string): Manga => {
+  const res = JSON.parse(data);
+  const c = res.comic;
+  const artist = res.artists.map((item: CKPerson) => item.name).join();
+  const author = res.authors.map((item: CKPerson) => item.name).join();
+  const relatedIds = c.relate_from
+    .map((item: CKRelated) => item.relate_to.slug);
+
+  const genres = res.genres
+    .map((item: CKGenre) => createTag({
+      label: item.name,
+      id: item.slug,
+    }));
+
+  return createManga({
+    id: mangaId,
+    titles: [ c.title ],
+    author,
+    artist,
+    relatedIds,
+    desc: c.desc.replaceAll("<br/>", "\n"),
+    image: c.cover_url,
+    rating: c.bayesian_rating,
+    status: c.status,
+    follows: c.user_follow_count,
+    hentai: c.hentai,
+    tags: [
+      createTagSection({
+        id: "genres",
+        label: "Genres",
+        tags: genres,
+      }),
+    ],
+  });
+};
+
+export const parseChapters =
+  (mangaId: string, data: string, settings: StateData): Chapter[] =>
+    JSON.parse(data)
+      .chapters
+      .filter((c: CKChapterPreview) =>
+        settings.filter.languages.includes(c.lang))
+      .map((c: CKChapterPreview): Chapter => {
+
+        const id = c.hid;
+        const name = c.title || "Unknown";
+        const volume = parseInt(c.vol || "") || 1;
+        const chapNum = parseInt(c.chap || "1") || 1;
+        const time = new Date(c.created_at);
+        const langCode = <LanguageCode> CKLanguages.getPBCode(c.lang);
+
+        return createChapter({
+          id,
+          mangaId,
+          name,
+          volume,
+          chapNum,
+          time,
+          langCode,
+        });
+      });
+
+export const parseSearchItems = (data: string): MangaTile[] =>
+  JSON.parse(data)
+    .map((item: CKSearchResult): MangaTile => {
+      let image = "https://comick.fun/static/failed_to_load.png";
+
+      if (item.cover_url) {
+        image = item.cover_url;
+      } else if (item.md_covers && item.md_covers[0]) {
+        image = item.md_covers[0].gpurl;
+      }
+
+      return createMangaTile({
+        id: item.slug,
+        image,
+        title: createIconText({ text: item.title }),
+      });
+    });
+
